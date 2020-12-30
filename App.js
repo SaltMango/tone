4e6882137ac14486792806fcd5fcf82130c7ac9f
Sreenath <mvsreenath@gmail.com>
2020-12-30 10:52:49 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+//import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar style="auto" />
+      <Text>Open up A!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
